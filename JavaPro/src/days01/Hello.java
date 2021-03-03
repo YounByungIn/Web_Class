95ce99836d97670182fab65c3b66cd3617ee561a
YounByungIn <74272979+YounByungIn@users.noreply.github.com>
2021-03-03 12:07:21 +0900

    Hi coding add.

--- JavaPro/src/days01/Hello.java
+++ JavaPro/src/days01/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		// 추가 코딩
+		System.out.println("Hi~");
 	}
 
 }
